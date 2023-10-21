3d26c81140dba86386ccc413b58e49203bd0c534
Luigi Soares <luigisp08@gmail.com>
2023-10-21 15:50:55 -0300

    Service layer, component registration

---
+++ src/main/java/com/luigisoares/project/services/UserService.java
@@ -0,0 +1,24 @@
+package com.luigisoares.project.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luigisoares.project.entities.User;
+import com.luigisoares.project.repositories.UserRepository;
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.get();
+	}
+}
